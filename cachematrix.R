83cf64c8cf036e3828fe6eb369315231a982aac3
candjmail <candjmail@gmail.com>
2014-09-18 21:41:37 -0500

    updated cache functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is Programming Assignment 2
+## It demonstrates the ability to cache data in accordance with
+## R scoping rules
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache 
+## its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function() {
+  ## orig and inv represent the original and inverted matrix
+      orig <- matrix()
+      inv <- matrix()
+      getorig <- function() orig
+      getinv <- function() inv
+      setorig <- function(x) orig <<- x
+      setinv <- function(x) inv <<- x
+      list(getorig = getorig, getinv = getinv, setorig = setorig,
+           setinv = setinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x, y,...) {
         ## Return a matrix that is the inverse of 'x'
+        ## First get the current state of original and inverted
+      myorig <- y$getorig()
+      myinv <- y$getinv()
+
+        ## if the original has changed, update everything
+     if (!identical(x, myorig)) {
+        y$setorig(x)
+        ##print("solving")
+        return(myinv <- y$setinv(solve(x)))
+      }
+        ## if the inverted exists use the cache
+      if (!is.null(myinv)) {
+        ##print("using cache")
+        return(myinv)
+      }
+        ## otherwise solve and cache the inversion
+      ##print("solving")
+      return(myinv <- y$setinv(solve(x)))
 }
